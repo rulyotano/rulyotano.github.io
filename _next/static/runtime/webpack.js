693bde5f887bc07e0480c20ea493b191b8f3449c
Raúl Otaño Hurtado <rulyotano@gmail.com>
2021-03-23 23:02:55 +0100

    v1.1.1 updated jobs

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "39c275ec9cb9a3e837eb";
+/******/ 	var hotCurrentHash = "70956df4fed68661533f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
